5bb643420a34f85709f78205d0c57d493fe938c8
PabloMarch <jpmarch4@gmail.com>
2024-04-09 21:19:55 -0600

    resolve graph challenges

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,30 @@
 const Stack = require('./stack');
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, vertex) {
+  if (!graph.adjacencyList[vertex]) return [];
+
+  const visited = new Set();
+  const stack = new Stack();
+  const results = [];
+
+  visited.add(vertex);
+  stack.push(vertex);
+  
+  while(!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+    const adjacentVertexs = graph.adjacencyList[currentVertex];
+
+    results.push(currentVertex);
+    
+    adjacentVertexs.forEach(adjVertex => {
+      if (!visited.has(adjVertex)) {
+        visited.add(adjVertex);
+        stack.push(adjVertex);
+      }
+    });
+  }
+
+  return results;
+}
 
 module.exports = depthFirstTraversal;
--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,27 @@
 const Queue = require('./queue');
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(graph, vertex) {
+  const visited = new Set();
+  const queue = new Queue();
+  const result = new Array();
+
+  visited.add(vertex);
+  queue.enqueue(vertex);
+
+  while (!queue.isEmpty()) {
+    const currentVertex = queue.dequeue();
+
+    result.push(currentVertex);
+
+    graph.adjacencyList[currentVertex].forEach(adjVertex => {
+      if (!visited.has(adjVertex)) {
+        visited.add(adjVertex);
+        queue.enqueue(adjVertex);
+      }
+    });
+  };
+
+  return result;
+}
 
 module.exports = breadthFirstTraversal;
